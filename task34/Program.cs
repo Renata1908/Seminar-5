8da35cd7530204736f829548a1de9180f674c8e5
Renata19 <renatalsa@mail.ru>
2022-10-04 20:50:08 +0700

    34

---
+++ task34/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int evenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count = count + 1;
+        }       
+    }
+    return count;
+}
+
+int[] array = GetArray(10, 100, 999);
+Console.WriteLine(string.Join(", ", array));
+int res=evenNumbers(array);
+Console.WriteLine($"Количество четных чисел в массиве = {res}: ");
